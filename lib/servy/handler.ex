a94f5ef055781b6986828178fcec2116a069bd42
doctorru <vjblan@gmail.com>
2017-10-11 00:10:33 -0300

    function clauses

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -3,9 +3,13 @@ defmodule Servy.Handler do
   def handle(request) do
     request 
     |> parse 
+    |> log
     |> route 
     |> format_response
   end
+
+
+  def log(conv), do: IO.inspect conv
 
 
   def parse(request) do
@@ -20,7 +24,15 @@ defmodule Servy.Handler do
 
 
   def route(conv) do
-     %{ conv | resp_body: "Bears, Lions, Tigers" }
+    route(conv, conv.method, conv.path)
+  end
+
+  def route(conv, "GET", "/wildthings") do
+    %{ conv | resp_body: "Bears, Lions, Tigers"}
+  end
+
+  def route(conv, "GET", "/bears") do
+    %{ conv | resp_body: "Teddy, Smokey, Paddington"}
   end
 
 
@@ -41,12 +53,31 @@ GET /wildthings HTTP/1.1
 Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
-
 """
 
 response = Servy.Handler.handle(request)
-
 IO.puts response
 
+
+request = """
+GET /bears HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+"""
+
+response = Servy.Handler.handle(request)
+IO.puts response
+
+
+request = """
+GET /bigfoot HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+"""
+
+response = Servy.Handler.handle(request)
+IO.puts response
 
 
